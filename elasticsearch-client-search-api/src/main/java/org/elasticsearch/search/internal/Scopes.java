9ee03f9adb95b53ae79c76ec5803fd7548023132
Jörg Prante <joergprante@gmail.com>
2012-10-06 17:33:43 +0200

    SNAPSHOT update

--- elasticsearch-client-search-api/src/main/java/org/elasticsearch/search/internal/Scopes.java
+++
@@ -1,7 +0,0 @@
-package org.elasticsearch.search.internal;
-
-public final class Scopes {
-        public static final String MAIN = "_main_";
-        public static final String GLOBAL = "_global_";
-        public static final String NA = "_na_";
-    }
